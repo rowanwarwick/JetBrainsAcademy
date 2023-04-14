b0a5d3db35dcc9acd577dcea18578616f44dd62a
Rowan Warwick <rowanwar@et-i6.kzn.21-school.ru>
2023-04-14 21:15:56 +0300

    words virtuos

--- chellange/WordsVirtuoso/src/main/kotlin/Main.kt
+++ chellange/WordsVirtuoso/src/main/kotlin/Main.kt
@@ -56,20 +56,33 @@ object Game {
             field = value
         }
     var dataWords = mutableListOf<String>()
+    private val hint = mutableSetOf<Char>()
+    private val allVariant = mutableListOf<String>()
 
     fun start() {
+        val startTime = System.currentTimeMillis()
+        var countRound = 1
         do {
             println("Input a 5-letter word:")
             val input = readln().lowercase()
+            if (input != "exit") println(workWithPlayerInput(input))
             when (input) {
-                randomWord -> println("Correct!")
+                randomWord -> println(
+                    "Correct!\n" +
+                            if (countRound != 1) "The solution was found after $countRound tries in ${System.currentTimeMillis() - startTime} seconds."
+                            else "Amazing luck! The solution was found at once."
+                )
+
                 "exit" -> println("The game is over.")
-                else -> println(subValue(input))
+                else -> {
+                    if (hint.isNotEmpty()) println("\u001B[48:5:14m${hint.sorted().joinToString("")}\u001B[0m")
+                    countRound++
+                }
             }
         } while (input != randomWord && input != "exit")
     }
 
-    private fun subValue(input: String): String {
+    private fun workWithPlayerInput(input: String): String {
         return when {
             input.length != 5 -> "The input isn't a 5-letter word."
             input.contains("[^a-zA-Z]".toRegex()) -> "One or more letters of the input aren't valid."
@@ -78,10 +91,15 @@ object Game {
             else -> {
                 val output = StringBuilder()
                 for ((index, char) in input.withIndex()) {
-                    if (char in randomWord) output.append(if (index == randomWord.indexOf(char)) char.uppercase() else char)
-                    else output.append("_")
+                    if (char in randomWord) {
+                        output.append(if (index == randomWord.indexOf(char)) "\u001B[48:5:10m${char.uppercase()}\u001B[0m" else "\u001B[48:5:11m${char.uppercase()}\u001B[0m")
+                    } else {
+                        output.append("\u001B[48:5:7m${char.uppercase()}\u001B[0m")
+                        hint.add(char.uppercaseChar())
+                    }
                 }
-                output.toString()
+                allVariant.add(output.toString())
+                allVariant.joinToString("\n", "\n")
             }
         }
     }
