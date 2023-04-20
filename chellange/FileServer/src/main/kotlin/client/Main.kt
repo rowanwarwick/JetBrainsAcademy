771135677d880eb3905cbea6f9b0364fb0fa68cb
Rowan Warwick <rowanwar@et-i6.kzn.21-school.ru>
2023-04-20 15:35:08 +0300

    FileServer

---
+++ chellange/FileServer/src/main/kotlin/client/Main.kt
@@ -0,0 +1,91 @@
+package client
+
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.File
+import java.net.InetAddress
+import java.net.Socket
+
+object Client {
+    init {
+        System.setProperty("client.data", "${System.getProperty("user.dir")}/src/main/kotlin/client/data/") //
+    }
+
+    private const val address = "127.0.0.1"
+    private const val port = 23456
+    private val socket = Socket(InetAddress.getByName(address), port)
+    private val input = DataInputStream(socket.getInputStream())
+    private val output = DataOutputStream(socket.getOutputStream())
+
+    private fun put() {
+        println("Enter name of the file")
+        val name = readln()
+        val filePath = System.getProperty("client.data") + name
+        if (File(filePath).exists()) {
+            val format = "[.].+".toRegex().findAll(name).last().value
+            val message = File(filePath).readBytes()
+            println("Enter name of the file to be saved on server:")
+            output.apply {
+                writeUTF("PUT $name $format ${readln()}")
+                writeInt(message.size)
+                write(message)
+            }.also { println("The request was sent.") }
+            val answer = input.readUTF().split(" ")
+            println(if (answer[0] == "200") "Response says that file is saved! ID = ${answer[1]}" else "The response says that creating the file was forbidden!")
+        } else {
+            println("File not found")
+        }
+    }
+
+    private fun get() {
+        println("Do you want to get the file by name or by id (1 - name, 2 - id):")
+        when (val user = readln()) {
+            "1", "2" -> {
+                println(if (user == "1") "Enter name of the file:" else "Enter id:")
+                output.writeUTF("GET $user${readln()}").also { println("The request was sent.") }
+                val answer = input.readUTF().split(" ")
+                if (answer[0] == "200") {
+                    val length = input.readInt()
+                    val message = ByteArray(length)
+                    input.readFully(message, 0, message.size)
+                    println("The file was downloaded! Specify a name for it:")
+                    File(System.getProperty("client.data") + readln() + answer[1]).writeBytes(message)
+                    println("File saved on the hard drive!")
+                } else {
+                    println("The response says that this file is not found!")
+                }
+            }
+
+            else -> println("Invalid input")
+        }
+    }
+
+    private fun delete() {
+        println("Do you want to delete the file by name or by id (1 - name, 2 - id):")
+        when (val user = readln()) {
+            "1", "2" -> {
+                println(if (user == "1") "Enter name of the file:" else "Enter id:")
+                output.writeUTF("DELETE $user${readln()}").also { println("The request was sent.") }
+                println(if (input.readUTF() == "200") "The response says that the file was successfully deleted!" else "The response says that the file was not found!")
+            }
+
+            else -> println("Invalid input")
+        }
+    }
+
+
+    fun query() {
+        println("Enter action (1 - get a file, 2 - save a file, 3 - delete a file):")
+        when (readln().uppercase()) {
+            "2" -> put()
+            "1" -> get()
+            "3" -> delete()
+            "EXIT" -> output.writeUTF("EXIT")
+        }
+        socket.close()
+    }
+}
+
+fun main() {
+    Client.query()
+}
---
+++ chellange/FileServer/src/main/kotlin/server/Main.kt
@@ -0,0 +1,124 @@
+package server
+
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.File
+import java.net.InetAddress
+import java.net.ServerSocket
+import java.net.Socket
+import java.net.SocketException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.random.Random
+import kotlin.system.exitProcess
+
+object Server {
+    private const val address = "127.0.0.1"
+    private const val port = 23456
+    private val server = ServerSocket(port, 50, InetAddress.getByName(address))
+    private val decoderFileName = mutableMapOf<Int, Pair<String, String>>()
+    private val executor: ExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+
+    init {
+        System.setProperty("server.data", "${System.getProperty("user.dir")}/src/main/kotlin/server/data/") //
+        if (File(System.getProperty("server.data") + "saveData").exists()) {
+            File(System.getProperty("server.data") + "saveData").forEachLine {
+                val data = it.split(" ")
+                decoderFileName[data[0].toInt()] = Pair(data[1], data[2])
+            }
+        }
+    }
+
+    private fun generate(): String {
+        val size = Random.Default.nextInt(1, 20)
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return (1..size).map { allowedChars.random() }.joinToString("")
+    }
+
+    private fun put(input: DataInputStream, output: DataOutputStream, fromUser: List<String>) {
+        val nameGenerate = generate()
+        val fileNameOnServer = System.getProperty("server.data") + fromUser.getOrElse(3) { nameGenerate }
+        if (!File(fileNameOnServer).exists()) {
+            var id = 0
+            val length = input.readInt()
+            val message = ByteArray(length)
+            input.readFully(message, 0, message.size)
+            File(fileNameOnServer + fromUser.getOrElse(2) { "" }).writeBytes(message)
+            while (decoderFileName.containsKey(id)) {
+                id++
+            }
+            decoderFileName[id] =
+                Pair(fromUser[1], fromUser.getOrElse(3) { nameGenerate } + fromUser.getOrElse(2) { "" })
+            output.writeUTF("200 $id")
+        } else {
+            output.writeUTF("403")
+        }
+    }
+
+    private fun get(output: DataOutputStream, fromUser: List<String>) {
+        val fileName = if (fromUser[1].first() == '1') {
+            fromUser[1].substring(1)
+        } else {
+            decoderFileName[fromUser[1].substring(1).toInt()]?.second
+        }
+        if (fileName != null && File(System.getProperty("server.data") + fileName).exists()) {
+            val format = "[.].+".toRegex().findAll(fileName).last().value
+            val message = File(System.getProperty("server.data") + fileName).readBytes()
+            output.apply {
+                writeUTF("200 $format")
+                writeInt(message.size)
+                write(message)
+            }
+        } else {
+            output.writeUTF("404")
+        }
+    }
+
+    private fun delete(output: DataOutputStream, fromUser: List<String>) {
+        val fileName = if (fromUser[1].first() == '1') {
+            fromUser[1].substring(1)
+        } else {
+            decoderFileName[fromUser[1].substring(1).toInt()]?.second
+        }
+        if (fileName != null && File(System.getProperty("server.data") + fileName).exists()) {
+            File(System.getProperty("server.data") + fileName).delete()
+            output.writeUTF("200")
+        } else {
+            output.writeUTF("404")
+        }
+    }
+
+    @Synchronized
+    private fun operation(socket: Socket) {
+        val input = DataInputStream(socket.getInputStream())
+        val output = DataOutputStream(socket.getOutputStream())
+        val fromUser = input.readUTF().trimEnd().split(" ")
+        when (fromUser[0]) {
+            "PUT" -> put(input, output, fromUser)
+            "GET" -> get(output, fromUser)
+            "DELETE" -> delete(output, fromUser)
+            "EXIT" -> {
+                executor.shutdown()
+                File(System.getProperty("server.data") + "saveData").writeText(decoderFileName.map { "${it.key} ${it.value.first} ${it.value.second}" }
+                    .joinToString("\n"))
+                server.close()
+            }
+        }
+        socket.close()
+    }
+
+    fun workServer() {
+        while (true) {
+            val socket = server.accept()
+            executor.submit { operation(socket) }
+        }
+    }
+}
+
+fun main() {
+    try {
+        Server.workServer()
+    } catch (_: SocketException) {
+        println("server/socket off")
+    }
+}
